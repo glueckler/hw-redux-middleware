c7839dac85bcf0e3b9fe6b082d9b6981cae3e287
Dean Glueckler <glueckler@gmail.com>
2018-10-07 12:52:22 -0700

    cleaded up index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route} from 'react-router-dom'
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from 'reducers'
 import Root from 'Root'
 
 import App from 'components/App'
